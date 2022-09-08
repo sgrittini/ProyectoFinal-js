2fabbc2226b23ff7f26dfb6ebf5c94ada27ff103
Sergio Grittini <106285902+sgrittini@users.noreply.github.com>
2022-09-08 10:59:28 -0300

    Entrega Proyecto Final

--- javaScript/fetch.js
+++ javaScript/fetch.js
@@ -1,13 +1,13 @@
 async function FetchSalon() {
 
-    //const resp = await fetch('/datoSalon.json');
-    const resp = await fetch('https://run.mocky.io/v3/a270a255-1a5d-43f1-be8d-045471bb5a9d');
+    const resp = await fetch('../datoSalon.json');
+    //const resp = await fetch('https://run.mocky.io/v3/a270a255-1a5d-43f1-be8d-045471bb5a9d');
     const data = await resp.json();
     return data;
 }
 
 async function FetchPrecios() {
-    const resp = await fetch('/listaPrecio.json');
+    const resp = await fetch('../listaPrecio.json');
     const data = await resp.json();
     return data;
 }
